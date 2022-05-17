c98d43f0aeefbaf5b98fbd72516acfe037642fb6
ahmetpalavan <ahmetpalavann@gmail.com>
2022-05-17 17:19:31 +0300

    operatorler.js

---
+++ operatorler.js
@@ -0,0 +1,26 @@
+let sayi1= 25;
+let sayi2= 7;
+
+let sonuc= sayi1 + sayi2;
+sonuc= sayi1 / sayi2;
+sonuc= sayi1 % sayi2;
+console.log(sonuc);
+
+let yas = '22';
+console.log(yas + 10);
+console.log(yas * 22);
+
+let yas1= 32;
+console.log(yas1 + 10);
+console.log( yas1 - 12);
+
+sayi2= sayi2 + 1
+sayi2++ ;
+console.log(sayi2);
+console.log("palavan adamdır");
+
+console.log("Sayı 1:" +sayi1);
+console.log(++sayi1)
+
+console.log("Sayı 2:" +sayi2);
+console.log(++sayi2);
