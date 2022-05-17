ce63170f956197d74ebf37d8ce290981e2dceec6
ahmetpalavan <ahmetpalavann@gmail.com>
2022-05-17 16:44:00 +0300

    veri_tipleri.js added

---
+++ calisma.js
@@ -0,0 +1,1 @@
+console.log("merhaba ahmet") ahnlasdkas.k
---
+++ veri_tipleri.js
@@ -0,0 +1,37 @@
+let sayi= 22;
+console.log(sayi);
+console.log(typeof sayi);
+
+let ondalikSayi= 22.123;
+console.log(ondalikSayi);
+console.log(typeof ondalikSayi);
+
+let negatifSayi= -212;
+console.log(negatifSayi);
+console.log(typeof negatifSayi);
+
+let isim= "Ahmet";
+let soyIsim="Palavan";
+console.log(soyIsim);
+console.log(typeof soyIsim);
+console.log(isim); 
+console.log(typeof isim);   
+
+let tamIsim= isim + ' '  + soyIsim;  
+console.log(tamIsim);
+
+let tamIsim2=`${isim} ${soyIsim}`;
+console.log(tamIsim2);
+
+let cumle="Ahmet, js öğreniyor.";
+console.log(cumle);
+console.log(typeof cumle);
+
+let asalSayiMi= true;
+console.log("Veri türümüzün değeri "  +  asalSayiMi  +  " veri tipi: " + typeof asalSayiMi);
+
+let yas= null;
+console.log(" değer: " + yas + " veri tipi: " + typeof yas);
+
+let renk;
+console.log(renk);
