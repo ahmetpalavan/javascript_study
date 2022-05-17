c5ea0efc1ef6327ecd78499a3301efb7d7796e5f
ahmetpalavan <ahmetpalavann@gmail.com>
2022-05-17 15:22:52 +0300

    project added

---
+++ console_alert_prompt.js
@@ -0,0 +1,16 @@
+let myAge= 22;
+console.log(myAge)
+console.warn('Dikkat')
+console.error('Yanlış İşlem')
+console.table({Ad:"Ahmet", Yaş:22});
+console.time("id")
+
+
+
+console.time("id")
+console.timeEnd("id")
+alert("Merhaba dünya")
+prompt("Bir sayı giriniz")
+let sayi= 31
+let girilenSayi= prompt("Bir sayı giriniz")
+console.log(girilenSayi)
---
+++ degisken_sabit_kavramlari.js
@@ -0,0 +1,14 @@
+var yasim= 34;
+console.log(yasim)  
+console.log(yasim)  
+console.log(yasim)  
+console.log(yasim)  
+console.log(yasim)  
+console.log(yasim)  
+console.log(34)
+
+let tcKimlik= 12344536
+console.log(tcKimlik)
+const piSayisi= 3.14
+console.log(piSayisi)
+
