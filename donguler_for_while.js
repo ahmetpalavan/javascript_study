a0254163e1615f848f72325d63440c260b463dec
ahmetpalavan <ahmetpalavann@gmail.com>
2022-05-19 22:31:55 +0300

    donguler_for_while.js

---
+++ donguler_for_while.js
@@ -0,0 +1,56 @@
+for (let a=0; a<2; a++){
+    console.log("adım ahmet");
+    console.log("memnun oldum");
+}
+    for (let a=3, b=8; a+b<30; a=a+4, b=b+1){
+    console.log("Döngü çalıştı");
+    console.log("a değeri: "+a+" b değeri: "+b+" ");
+}   
+// fordan sonra noktalı virgül konmaz
+        for (let a=0; a<10; a++){
+            console.log("Seni seviyorum"  );
+        }
+
+                let sayi=12;
+                while(sayi<22){
+                    console.log("Ahmet Palavan");
+                    sayi++;
+                            }
+                            
+        for(let a=15; a<22; a++){
+            console.log("Merhaba");
+                                    }
+            let yas=20;
+            do{
+                console.log("Ahmet Bey");
+                yas++;
+
+                } while(yas<50);
+
+            for(let i=1; i<21; i++){
+                if(i==15){
+                    break;
+                }
+            console.log("Break kullanımı");
+            }
+                for(let a=1; a<20; a++) {
+                    if(a==11){
+                        continue;
+                    }
+                    console.log("continue öğrenimi"+a);
+            }
+                
+                function furkan(){
+                    for(let furkan=31; furkan<69; furkan++) {
+                    if(furkan==62){
+                        return;
+                    }
+
+                    console.log("Return öğrenimi"+furkan);
+                    
+                }}
+
+                
+
+                    
+                
