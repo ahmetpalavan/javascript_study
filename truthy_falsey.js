13b334a93249bc9d08360f60c723e1ab5287a695
ahmetpalavan <ahmetpalavann@gmail.com>
2022-05-19 20:16:45 +0300

    truthy_falsey.js

---
+++ truthy_falsey.js
@@ -0,0 +1,15 @@
+let yas= 22;
+yas=11;
+
+let isim="ahmet";
+isim=''
+isim=null;
+isim=undefined;
+
+let dogumYili=2000;
+dogumYili="ahmet";
+    if(dogumYili * isim){
+        console.log("bu true ifadedir");
+    } else{
+        console.log("bu false ifadedir");
+    }
