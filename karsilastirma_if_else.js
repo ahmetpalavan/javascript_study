664e12105161eadf8593f224c55db0a6134fcd95
ahmetpalavan <ahmetpalavann@gmail.com>
2022-05-19 19:41:52 +0300

    karsilastirma_if_else.js

---
+++ karsilastirma_if_else.js
@@ -0,0 +1,22 @@
+let benimYasim=22, furkaninYasi=25;
+if(benimYasim<furkaninYasi){
+
+    console.log("Ben senden küçüğüm");
+
+
+} else{ 
+    console.log("ben senden büyüğüm");
+    
+}
+
+let a=20, b=25, c=25;
+if(a>b) {
+    c=a+b}
+    else{
+        c=a-b;
+    }
+    console.log("c değeri:" +c);
+    
+    //ternary operatör
+    c=(a<b) ? (a+b) : (a-b);
+    console.log("c'nin yeni değeri:"+c);
