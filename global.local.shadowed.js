412f25e6c5637c2adaa0e0a6272b365c7f2d2c2b
ahmetpalavan <ahmetpalavann@gmail.com>
2022-05-31 18:35:18 +0300

    global.local.shadowed.js

---
+++ global.local.shadowed.js
@@ -0,0 +1,13 @@
+let isim="Ahmet"
+let yas = "22"
+selamVer()
+function selamVer() {
+    console.log("İsim:" + isim, "Yaş:" + yas);
+}
+
+// let isim= "Furkan"
+// let yas = "22"
+// sananeAlis()
+// function sananeAlis() {
+//     console.log("İsim:" + isim, "Yaş:" + yas)
+// }
