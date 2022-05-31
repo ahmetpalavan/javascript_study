580982c17cee7a6c5b79469119828caa59c31239
ahmetpalavan <ahmetpalavann@gmail.com>
2022-05-31 17:55:53 +0300

    fonksiyon.kavrami.js

---
+++ fonksiyon.kavrami.js
@@ -0,0 +1,31 @@
+// belliAraliklardakiSayilariTopla  (0, 21);
+// belliAraliklardakiSayilariTopla (40,48);
+
+// function belliAraliklardakiSayilariTopla (baslangicSayisi, bitisSayisi) {
+//     let toplam= 0;
+//     for(let i=baslangicSayisi; i<=bitisSayisi; i++){
+//     toplam= toplam + i; }
+
+// console.log("Toplam:" + toplam); }
+
+// belliAraliktakiSayilariTopla (12,75)
+// belliAraliktakiSayilariTopla (31,62)
+
+// function belliAraliktakisSayilariTopla (baslangicSayisi, bitisSayisi){
+// let toplam= 0;
+// for (let i=baslangicSayisi; i<=bitisSayisi; i++){
+//     toplam= toplam + i
+// }
+
+// console.log("Toplam : " + toplam);}
+
+belliAraliktakiSayilariTopla(20,111)
+belliAraliktakiSayilariTopla (31,123)
+
+function belliAraliktakiSayilariTopla(baslangicSayisi, bitisSayisi){
+    let toplam=1;
+    for (let i=baslangicSayisi; i<=bitisSayisi; i++){
+        toplam= toplam + i
+    }
+    console.log("Toplam:" +toplam);
+}
