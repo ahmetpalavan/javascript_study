0d0c4b093bd0d1f7cac0efe7080b83e7b82be73c
ahmetpalavan <ahmetpalavann@gmail.com>
2022-06-07 18:31:38 +0300

    es6_fat_arrow.js

---
+++ es6_fat_arrow.js
@@ -0,0 +1,35 @@
+// const sananeAhmetSanane = function() {
+//     console.log("Merhaba Ahmet");
+// }
+// console.log(sananeAhmetSanane());
+
+// function selamVer(){
+//     console.log("Merhaba");
+// }
+// selamVer()
+
+// const senAdamMisinYa = function() {
+//     console.log("Furkan sen adam mısın ya?");
+// }
+// senAdamMisinYa()
+
+// const fatArrow = () => {
+//     console.log("En Büyük Fenerbahçe");
+// }
+// fatArrow()
+
+function karesiniAl (sayi){
+    return sayi * sayi ;
+}
+console.log(karesiniAl(1907));
+
+const sayilariTopla =(s1, s2) => s1+ s2;
+console.log(sayilariTopla(25, 26));
+
+const fatArrow = sayi => sayi / sayi
+console.log(fatArrow(15));
+
+const fatArrowParametre =(sayi) =>{
+return sayi * sayi
+}
+console.log(fatArrowParametre(165));
